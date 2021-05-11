377afd93ace028287e8651919b7972ad4fe28a3b
gerardambe <gerardambe@yahoo.com>
2021-05-11 06:13:02 +0000

    055 - Deployment part 1

--- bossman_auction/settings.py
+++ bossman_auction/settings.py
@@ -120,21 +120,14 @@ WSGI_APPLICATION = 'bossman_auction.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
 
 DATABASES = {
-    'default': dj_database_url.parse('postgres://pphfltsbitapgt:2165f3801ed33a25fa13bc267f991e0f1f4aa7685eb4f38f5e9fa169495056d3@ec2-34-254-69-72.eu-west-1.compute.amazonaws.com:5432/ddhd1ja82faa85')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
